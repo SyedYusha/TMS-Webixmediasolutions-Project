6904fd839eb0d4dabb73f58eb31244a168a086a6
Your Name <your.email@example.com>
2025-09-25 14:41:48 +0500

    Signin validation [Updated]

--- SCRIPT[files]/signin.js
+++ SCRIPT[files]/signin.js
@@ -1,55 +1,23 @@
 let popup = document.getElementById("pop-up");
-let userId = document.getElementById("user-id");
-let password = document.getElementById("password");
+let popup_invalid = document.getElementById("pop-up-invalid");
 let adminId = "yusha@gmail.com";
 
+
 function proceed() {
-  popup.classList.add("show-popup");
-  popup.classList.remove("close-pop-up");
- }
- function close_popup() {
-     popup.classList.remove("show-popup");
-     popup.classList.add("close-pop-up");
+    var userId = document.getElementById("user-id").value;
+    var password = document.getElementById("password").value;
+    if(userId == "admin@tms" && password == "admin@2020") {
+        popup.classList.add('show-popup');
+        popup_invalid.classList.add('close-popup');
     }
-    setTimeout(close_popup, 3000);
-if (userId.value === adminId) {
-  proceed();
-} else {
-//   alert("Incorrect username/password");
+    if(userId == "" && password == "");{
+        popup_invalid.classList.add('show-popup');
+    }
 }
-// const resumeBtns = document.querySelectorAll('.btn_resume');
-
-// resumeBtns.forEach((btn, idx)=>{
-//     btn.addEventListener('click', ()=>{
-//         const resumeDet = document.querySelectorAll('.resume-details')
-//         resumeBtns.forEach(btn =>{
-//             btn.classList.remove('active');
-//         });
-//         btn.classList.add('active');
-//         resumeDet.forEach(detail =>{
-//             detail.classList.remove('active');
-//         });
-//         resumeDet[idx].classList.add('active');
-//     });
-// });
-
-
-const change_Card = document.getElementsByClassName('card-header-list')
-const change_Card2 = document.getElementsByClassName('card-header-list-2')
-const change_Card3 = document.getElementsByClassName('card-header-list-3')
-
-function change_card() {
-    change_Card.classList.add('active')
-    change_Card.classList.remove('active')
+function tryagain_popup() {
+    popup.classList.remove('show-popup');
+    popup_invalid.classList.remove('show-popup');
 }
-change_card();
-function change_card_2() {
-    change_Card2.classList.add('active')
-    change_Card.classList.remove('active')
+function proceed_panel() {
+    window.location.assign = '../Admin Dashboard index files';
 }
-change_card_2();
-function change_card_3() {
-    change_Card3.classList.add('active')
-    change_Card2.classList.remove('active')
-}
-change_card_3();
