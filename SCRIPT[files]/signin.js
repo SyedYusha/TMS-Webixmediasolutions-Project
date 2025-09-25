9fe7a671b96bcd78eb16bce7091920595bac2915
Your Name <your.email@example.com>
2025-09-25 22:43:37 +0500

    Signin validation done Driver Dashboard start

--- SCRIPT[files]/signin.js
+++ SCRIPT[files]/signin.js
@@ -2,22 +2,46 @@ let popup = document.getElementById("pop-up");
 let popup_invalid = document.getElementById("pop-up-invalid");
 let adminId = "yusha@gmail.com";
 
-
 function proceed() {
-    var userId = document.getElementById("user-id").value;
-    var password = document.getElementById("password").value;
-    if(userId == "admin@tms" && password == "admin@2020") {
-        popup.classList.add('show-popup');
-        popup_invalid.classList.add('close-popup');
-    }
-    if(userId == "" && password == "");{
-        popup_invalid.classList.add('show-popup');
-    }
+  var userId = document.getElementById("user-id").value;
+  var password = document.getElementById("password").value;
+  function proceed_panel() {
+    window.location.href =
+      "file:///D:/Web%20Development/Sigma%20Web%20development%20Project/Project%2309-TMS-Webixmediasolutions/Admin%20Dashboard%20index%20files/dashboard_admin.html";
+  }
+  if (userId == "admin@tms" && password == "admin@2020") {
+    popup.classList.add("show-popup");
+    popup_invalid.classList.add("close-popup");
+    proceed_panel();
+  }
+  if (userId == "" && password == "");
+  {
+    popup.classList.add(".show-popup");
+    popup_invalid.classList.add("show-popup");
+  }
+}
+function proceed() {
+  var userId = document.getElementById("user-id").value;
+  var password = document.getElementById("password").value;
+  function proceed_panel() {
+    window.location.href =
+      "file:///D:/Web%20Development/Sigma%20Web%20development%20Project/Project%2309-TMS-Webixmediasolutions/Driver%20Dashboard%20index%20Files/index.html";
+  }
+  if (userId == "driver@tms" && password == "dri@2020") {
+    popup.classList.add("show-popup");
+    popup_invalid.classList.add("close-popup");
+    proceed_panel();
+  }
+  if (userId == "" && password == "");
+  {
+    popup.classList.add(".show-popup");
+    popup_invalid.classList.add("show-popup");
+  }
 }
 function tryagain_popup() {
-    popup.classList.remove('show-popup');
-    popup_invalid.classList.remove('show-popup');
+  popup.classList.remove("show-popup");
+  popup_invalid.classList.remove("show-popup");
 }
-function proceed_panel() {
-    window.location.assign = '../Admin Dashboard index files';
-}
+// function proceed_panel() {
+//     window.location.href = 'file:///D:/Web%20Development/Sigma%20Web%20development%20Project/Project%2309-TMS-Webixmediasolutions/Admin%20Dashboard%20index%20files/dashboard_admin.html';
+// }
